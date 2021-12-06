0beed0c82e5cf0f7abd721d9800bf3687729f28c
carolay09 <carolaycaipo@outlook.com>
2021-12-06 01:57:04 -0500

    solucion de conflicto en las rutas

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use App\Http\Controllers\DetailSaleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-<<<<<<< HEAD
 use App\Http\Controllers\CategoryController;
-=======
 use App\Http\Controllers\SaleController;
->>>>>>> f442f7dcc60aa5b19848222a85f60d88785d6b62
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -28,16 +25,14 @@ Route::post('dashboard/products/update', [ProductController::class, 'store_admi'
 Route::get('dashboard/products/{id}/edit', [ProductController::class, 'edit_admi'])->name('products.edit.admi');
 Route::patch('dashboard/products/{product}', [ProductController::class, 'update_admi'])->name('products.update.admi');
 Route::resource('products', ProductController::class);
-<<<<<<< HEAD
+
 
 Route::resource('categories', CategoryController::class);
 
-Route::get('/', function(){
-    return view('welcome');
-});
-=======
+
+
 Route::resource('detail_sales', DetailSaleController::class);
->>>>>>> f442f7dcc60aa5b19848222a85f60d88785d6b62
+
 
 
 
