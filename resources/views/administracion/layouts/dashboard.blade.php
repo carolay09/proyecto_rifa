954274c7d88eeaee4410404fd6c4103234ee4337
manuelesp <manuel2601.41@gmail.com>
2021-12-07 10:51:44 -0500

    solucion de conflicto en dashboard

--- resources/views/administracion/layouts/dashboard.blade.php
+++ resources/views/administracion/layouts/dashboard.blade.php
@@ -2,30 +2,6 @@
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>@yield('title')</title>
-  <!-- plugins:css -->
-  {{-- <link rel="stylesheet" href="vendors/feather/feather.css">
-  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="vendors/typicons/typicons.css">
-  <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
-  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css"> --}}
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  {{-- <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <link rel="stylesheet" href="js/select.dataTables.min.css"> --}}
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="{{asset('css/vertical-layout-light/style.css')}}">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> --}}
-  <!-- endinject -->
-  {{-- <link rel="shortcut icon" href="images/favicon.png" /> --}}
-=======
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,8 +21,6 @@
     <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
-
->>>>>>> 76b72dcaf9995a3236fc3e778356e93fc154f871
 </head>
 
 <body id="page-top">
@@ -141,40 +115,7 @@
             </div>
 
         </ul>
-<<<<<<< HEAD
-      </nav>
-      <!-- partial aqui va el contenido -->
-      @yield('content')
-      <!-- main-panel ends -->
-    </div>
-    <!-- page-body-wrapper ends -->
-    
-  </div>
-  
-  <!-- container-scroller -->
 
-  <!-- plugins:js -->
-  <script src="{{asset('js/vendor.bundle.base.js')}}"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  {{-- <script src="vendors/chart.js/Chart.min.js"></script>
-  <script src="vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-  <script src="vendors/progressbar.js/progressbar.min.js"></script> --}}
-
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="{{asset('js/off-canvas.js')}}"></script>
-  <script src="{{asset('js/hoverable-collapse.js')}}"></script>
-  {{-- <script src="js/template.js"></script>
-  <script src="js/settings.js"></script>
-  <script src="js/todolist.js"></script> --}}
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  {{-- <script src="js/dashboard.js"></script>
-  <script src="{{asset('js/dashboard.js')}}"></script>
-  <script src="js/Chart.roundedBarCharts.js"></script> --}}
-  <!-- End custom js for this page-->
-=======
         <!-- End of Sidebar -->
 
         @yield('content')
@@ -198,7 +139,7 @@
     <script src="{{asset('js/chart-area-demo.js')}}"></script>
     <script src="{{asset('js/chart-pie-demo.js')}}"></script>
 
->>>>>>> 76b72dcaf9995a3236fc3e778356e93fc154f871
+
 </body>
 
 </html>
